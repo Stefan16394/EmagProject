136073646cd1439b81c8017a5435c0a1b2b11e51
Ivana Vasileva <ivaneerha@gmail.com>
2019-02-01 19:28:03 +0200

    Some corrections.

--- src/application/App.java
+++ src/application/App.java
@@ -36,30 +36,29 @@ public class App {
 		this.userStorage.registerUser(UserFactory.createUser("admin@emag.bg", "admin", true));
 
 		while (true) {
-			if(this.currentUser != null) {
-				if(this.currentUser.isAdmin()) {
+			if (this.currentUser != null) {
+				if (this.currentUser.isAdmin()) {
 					System.out.println("1 - Add product, 2 - Delete product, 3 - Log out");
 					String command = sc.nextLine();
-					switch(command) {
-						case "1": 
-							this.productService.createProduct(this.productStorage);
-							break;
-						case "3":
-							if(this.currentUser != null) {
-								this.currentUser = null;
-								System.out.println("You logged out successfully!");
-							} else {
-								System.out.println("First you need to log in!");
-							}
-							break;
-						default: 
-							System.out.println("Invalid command!");
-							break;
+					switch (command) {
+					case "1":
+						this.productService.createProduct(this.productStorage);
+						break;
+					case "3":
+						if (this.currentUser != null) {
+							this.currentUser = null;
+							System.out.println("You logged out successfully!");
+						} else {
+							System.out.println("First you need to log in!");
+						}
+						break;
+					default:
+						System.out.println("Invalid command!");
+						break;
 					}
 				}
 			} else {
-				System.out.println(
-						"Enter command: 1 - register User, 2 - log in, 3 - list users, "
+				System.out.println("Enter command: 1 - register User, 2 - log in, 3 - list users, "
 						+ "4 - Find Product By Category, 5 - Delete account, 6 - Log out, 7 - Add to your cart,"
 						+ " 8 - Check out the things in your cart");
 				String command = sc.nextLine();
@@ -81,14 +80,14 @@ public class App {
 					this.productService.findProductsByCategory(this.productStorage);
 					break;
 				case "5":
-					if(this.currentUser != null) {
+					if (this.currentUser != null) {
 						this.userService.deleteAccount(this.userStorage, this.currentUser);
 					} else {
 						System.out.println("First you need to log in!");
 					}
 					break;
 				case "6":
-					if(this.currentUser != null) {
+					if (this.currentUser != null) {
 						this.currentUser = null;
 						System.out.println("You logged out successfully!");
 					} else {
@@ -101,39 +100,42 @@ public class App {
 				}
 				System.out.println();
 
-		this.productService.generateRandomProducts(this.productStorage);
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		this.productStorage.listAllProducts();
-		
-		while(true) {
-			System.out.println("Enter command: 1 - register User, 2 - log in, 3 - list users, 4 - Add Product, 5 - Find Product By Category");
-			String command = sc.nextLine();
-			switch (command) {
-			case "1":
-				this.userService.register(this.userStorage);
-				break;
-			case "2":
-				User user = this.userService.login(this.userStorage);
-				if(user!=null) {
-					this.currentUser = user;
-				}
-				System.out.println(this.currentUser.getEmail());
-				break;
-			case "3":
-				userStorage.listUsers();
-				break;
-			case "4":
-				this.productService.createProduct(this.productStorage);
-				break;
-			case "5":
-				this.productService.findProductsByCategory(this.productStorage);
-				break;
-			default:
-				System.out.println("Invalid command!");
+				this.productService.generateRandomProducts(this.productStorage);
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				this.productStorage.listAllProducts();
 
+				while (true) {
+					System.out.println(
+							"Enter command: 1 - register User, 2 - log in, 3 - list users, 4 - Add Product, 5 - Find Product By Category");
+					String command2 = sc.nextLine();
+					switch (command2) {
+					case "1":
+						this.userService.register(this.userStorage);
+						break;
+					case "2":
+						User user = this.userService.login(this.userStorage);
+						if (user != null) {
+							this.currentUser = user;
+						}
+						System.out.println(this.currentUser.getEmail());
+						break;
+					case "3":
+						userStorage.listUsers();
+						break;
+					case "4":
+						this.productService.createProduct(this.productStorage);
+						break;
+					case "5":
+						this.productService.findProductsByCategory(this.productStorage);
+						break;
+					default:
+						System.out.println("Invalid command!");
+
+					}
+				}
 			}
 		}
 	}
